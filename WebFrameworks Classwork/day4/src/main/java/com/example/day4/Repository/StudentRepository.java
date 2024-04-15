e1709b4bd4802c22337c39c52b5a12eee9f615dc
Akshaya810 <151520139+Akshaya810@users.noreply.github.com>
2024-04-15 09:42:31 +0530

    Delete WebFrameworks Classwork/day4 directory

--- WebFrameworks Classwork/day4/src/main/java/com/example/day4/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.day4.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day4.Model.StudentModel;
-
-public interface StudentRepository extends JpaRepository <StudentModel, Integer>{
-
-}
